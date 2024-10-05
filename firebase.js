9598d0c5b0e4eb252b12d556c9c4d8086c368791
Joao19822 <jv261555@gmail.com>
2024-10-04 22:04:42 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,67 +1,120 @@
-let frm = document.getElementById('frm_cadastro');
-const url_api = "https://devup-tmp02-default-rtdb.firebaseio.com/"
+var frm = document.getElementById('frm_cadastro');
+const url_api = "https://devup-tmp02-default-rtdb.firebaseio.com/";
 
-frm.addEventListener('submit', e=>{
+let editId = null;
+frm.addEventListener('submit', e => {
     e.preventDefault();
 
-    let nome = document.querySelector('#nome')
-    let telefone = document.querySelector('#telefone')
-    let email = document.querySelector('#email')
+    let nome = document.querySelector('#nome');
+    let telefone = document.querySelector('#telefone');
+    let email = document.querySelector('#email');
 
     let dados = {
         'nome': nome.value,
         'telefone': telefone.value,
-        'email': email.value
+        'email': email.value,
+    };
+
+    if (id_usuario.value) {
+        //atualização
+        fetch (url_api + 'usuario/' + id_usuario.value + '.json', {
+            method: 'PUT',
+            body: JSON.stringify(dados)
+        })
+            .then(resposta => resposta.json())
+            .then(dados => {
+                frm.reset()
+                listar();
+            })
+    } else {
+        //cadastro
+        fetch(url_api + 'usuario.json', {
+            method: 'POST',
+            body: JSON.stringify(dados)
+        })
+            .then(resposta => resposta.json())
+            .then(dados => {
+                frm.reset()
+                listar();
+            })
     }
 
 
-    fetch(url_api + 'usuario.json',{
-        method: 'POST',
-        body: JSON.stringify(dados)
-    })
-})
+    listar();
+    frm.reset
+    id_usuario.value = ""
 
-function listar(){
-    let lista = document.getElementById('lista')
-    fetch(url_api+'usuario.json')
-    .then(resposta => resposta.json())
-    .then(dados=>{
-        lista.innerHTML=""
-        for (const key in dados) {
-            lista.innerHTML +=
-            `
+    document.querySelector('button[type=submit').innerHTML = 'Cadastrar'
+});
+
+function listar() {
+    let lista = document.getElementById('lista');
+    fetch(url_api + 'usuario.json')
+        .then(resposta => resposta.json())
+        .then(dados => {
+            lista.innerHTML = "";
+            for (const key in dados) {
+                lista.innerHTML += `
             <tr>
-            <td>${dados[key].nome}</td>
-            <td>${dados[key].telefone}</td>
-            <td>${dados[key].email}</td>
-            <td> <button class="btn btn-warning">E</td></button>
-            <td> <button class="btn btn-danger">D</td></button>
-            </tr>`
-        }
-    })
+                <td>${dados[key].nome}</td>
+                <td>${dados[key].telefone}</td>
+                <td>${dados[key].email}</td>
+                <td>
+                    <button class="btn btn-warning" onclick="editar ('${key}')"><i class="bi bi-pencil-square"></i></button>
+                    <button class="btn btn-danger" onclick="deletar('${key}')"><i class="bi bi-person-x-fill"></i></button>
+                </td>
+            </tr>
+            `;
+            }
+        });
 }
+
+function editar(id_usuario) {
+    fetch(url_api + 'usuario/' + id_usuario + '.json')
+        .then(resposta => resposta.json())
+        .then(dados => {
+            nome.value = dados.nome
+            email.value = dados.email
+            telefone.value = dados.telefone
+            document.getElementById('id_usuario').value = id_usuario
+
+            document.querySelector('button[type=submit]').innerHTML = 'ALTERAR'
+        })
+}
+
+function deletar(id_usuario) {
+{fetch(url_api + 'usuario/' + id_usuario + '.json', {
+        method: 'DELETE',
+    })
+        .then(resposta => resposta.json()) 
+        .then(dados =>{
+            listar();
+        })
+    }
+}
+
 
 listar();
 
+setInterval(() => {
+    listar();
+}, 1000);
 
+function exportar(id_tabela) {
+    var nome_arquivo_exportacao = `${id_tabela}.xlsx`;
+    var tabela_exportacao = document.getElementById(id_tabela);
+    var planilha = XLSX.utils.table_to_book(tabela_exportacao);
+    XLSX.writeFile(planilha, nome_arquivo_exportacao);
+}
 
-fetch('https://devup-tmp02-default-rtdb.firebaseio.com/' + dados, {
-    method: 'DELETE',
-  })
-  .then(resposta => resposta.text())
-  .then(resposta => console.log(resposta))
-
-
-// function deletar(){
-
-//     let deletar = document.getElementById('frm_cadastro')
-
-//     fetch(url_api + 'usuario.json')
-//        .then(resposta => resposta.json())
-//        .then(dados=>{
-//         method: 'DELETE',
-//         body: JSON.stringify(dados)
-//        })
-    
-// }
-
+function gerar_pdf() {
+    var element = document.querySelector('.container');
+    var opt = {
+        margin: 1,
+        filename: 'arquivo.pdf',
+        image: {type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: {unit: 'in', format:'letter', orientation: 'portrait' }
+    };
+    html2pdf().set(opt).from(element).save();
+}
